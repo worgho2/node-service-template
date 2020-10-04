ab30d4cade41c3763e0ea9ff7cb834c59c4f7da4
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-10-04 20:22:36 -0300

    Added files on interfaces layer

---
+++ api/lib/interfaces/express-adapters/express-router-adapter.js
@@ -0,0 +1,21 @@
+class ExpressRouterAdapter {
+  static adapt(route) {
+    return async (req, res, next) => {
+      const httpRequest = {
+        params: req.params,
+        headers: req.headers,
+        body: req.body,
+        props: req.props,
+      }
+
+      try {
+        const httpResponse = await route(httpRequest)
+        res.status(httpResponse.statusCode).json(httpResponse.body)
+      } catch (error) {
+        next(error)
+      }
+    }
+  }
+}
+
+export default ExpressRouterAdapter
