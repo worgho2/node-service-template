752bb47b224f8a8410919ddc570ba789cd91572b
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-10-06 22:15:18 -0300

    Added infrastructure services template

---
+++ api/lib/infrastructure/firebase/firebase-admin-facade.js
@@ -0,0 +1,13 @@
+import { config } from './config'
+
+class FirebaseAdminFacade {
+  static setup(configFile = config) {
+    return
+  }
+
+  async verifyToken({ idToken }) {
+    return null
+  }
+}
+
+export default FirebaseAdminFacade
